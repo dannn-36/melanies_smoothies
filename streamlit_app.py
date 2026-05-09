891d5e5dde662cd5d08cd67c8b6b6d8c2553e3e2
Daniel <118573956+dannn-36@users.noreply.github.com>
2026-05-09 12:36:59 -0500

    Fix URL concatenation for API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ if ingredientes_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_on
+            f"https://my.smoothiefroot.com/api/fruit/ + {search_on}"
         )
 
         st.dataframe(
