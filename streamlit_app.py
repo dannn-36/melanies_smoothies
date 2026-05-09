c562c236a1eeb66c4c08a2216c23f304f59b2263
Daniel <118573956+dannn-36@users.noreply.github.com>
2026-05-09 12:08:10 -0500

    nuevo

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,6 @@ ingredientes_list = st.multiselect(
     fruit_list
 )
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
 
 if ingredientes_list:
 
@@ -56,3 +53,7 @@ if ingredientes_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
+        
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
