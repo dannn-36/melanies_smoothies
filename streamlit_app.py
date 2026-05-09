bdd97372e7593329020a2fec275eaaac7672d4ea
Daniel <118573956+dannn-36@users.noreply.github.com>
2026-05-09 12:30:32 -0500

    Add SEARCH_ON column to fruit options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,9 @@ session = cnx.session()
 # Get fruits
 my_dataframe = session.table(
     "smoothies.public.fruit_options"
-).select(col('FRUIT_NAME'))
+).select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 # Convert dataframe to python list
 fruit_list = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
